de8ff277e5c94a216f02e8a2db3c7963ebcda9a8
Alexandre Caulier <alexandre.caulier@protonmail.com>
2020-01-25 20:24:36 +0100

    Split main

---
+++ src/utils.rs
@@ -0,0 +1,39 @@
+use std::fs::{File, remove_file};
+use std::path::Path;
+use std::process::{Command, Stdio};
+
+use failure::Fallible;
+use rustfst::DrawingConfig;
+use rustfst::fst_traits::SerializableFst;
+use rustfst::semirings::SerializableSemiring;
+
+pub fn generate_image<P: AsRef<Path>, F: SerializableFst>(
+    path_images: P,
+    fst: &F,
+    name: &str,
+    config: &DrawingConfig,
+) -> Fallible<()>
+    where
+        F::W: SerializableSemiring,
+{
+    let path_images = path_images.as_ref();
+    let path_dot_file = path_images.join(format!("{}.dot", name));
+    fst.draw(&path_dot_file, &config)?;
+
+    let outputs = File::create(path_images.join(format!("{}.svg", name)))?;
+    Command::new("dot")
+        .args(&[
+            "-Tsvg",
+            path_dot_file
+                .as_os_str()
+                .to_str()
+                .unwrap(),
+        ])
+        .stdout(Stdio::from(outputs))
+        .spawn()?
+        .wait_with_output()?;
+
+    remove_file(path_dot_file)?;
+
+    Ok(())
+}
